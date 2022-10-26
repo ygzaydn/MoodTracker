cb67c364501bc60844a9b273d72e7ccff04e758c
ygzaydn <ygzaydns@gmail.com>
2022-10-26 14:41:13 +0300

    finalized

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
 
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { groupBy } from 'lodash';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.mood, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Analytics</Text>
+      <VictoryPie data={data} />
     </View>
   );
 };
